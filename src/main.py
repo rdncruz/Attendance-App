de9a56eae24ee20bc997adae9b6c91d535a0eb7c
rdncruz <raziele.cruz@wradvisory.co>
2025-04-22 17:51:02 +0800

    commit

--- src/main.py
+++ src/main.py
@@ -85,7 +85,7 @@ def main(page: ft.Page):
         if current_time >= "18:01:00":
             btn_clock.text = "Time-Out"
         elif current_time >= "00:01:00":
-            btn_clock.text = "Time-ins"
+            btn_clock.text = "Time-in"
 
         page.update()
         time.sleep(1)

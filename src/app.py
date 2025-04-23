6b4949cbde1acadfc47ad02e07be3fd267b9d3e1
rdncruz <raziele.cruz@wradvisory.co>
2025-04-23 10:49:58 +0800

    Establish API connection and enable data insertion to Google Sheets using gspread

--- src/app.py
+++ src/app.py
@@ -1,9 +1,32 @@
 import gspread
+from gspread_formatting import (
+    set_data_validation_for_cell_range,
+    DataValidationRule,
+    BooleanCondition
+)
 
 gc = gspread.service_account(filename='../src/credentials.json')
 
 sh = gc.open_by_key("1X_OgCH1dKqftUKEPXluDsUk6plmh8B0nvIkWyI-BiKs")
 
-sh.sheet1.update_acell('B1', 'Biss!')
+ws = sh.sheet1
 
-print(sh.sheet1.get('A1'))
+rule = DataValidationRule(
+    condition=BooleanCondition('NUMBER_GREATER', ['10']),
+    inputMessage='Value must be greater than 10',
+    
+    strict=True,
+    showCustomUi=True
+)
+ws.update_acell('A1', '5')
+#set_data_validation_for_cell_range(ws, 'A1', rule)
+
+
+#value_to_insert = 5  # Example value
+
+# Check if the value is greater than 1
+#if value_to_insert > 10:
+    #ws.update(range_name='A1', values=[[value_to_insert]])
+    #print(f'Value {value_to_insert} successfully inserted into A1.')
+#else:
+    #print('Error: Value must be greater than 10.')
--- src/main.py
+++ src/main.py
@@ -9,18 +9,23 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
+    #Gspread API CONNECTION
+    service_acc = gspread.service_account(filename='C:/Users/crenz/Documents/GitHub/Attendance-App/src/credentials.json')
+    key = service_acc.open_by_key("1X_OgCH1dKqftUKEPXluDsUk6plmh8B0nvIkWyI-BiKs")
+    worksheet = key.sheet1
+
+    #LOCAL VARIABLE
     txt = ft.Text(value = "Good Day. Please be on Time!!", size = 50)
-    txt_name = ft.TextField(label = "Name", width = 300 )
-    run_date = ft.TextField(label = "Date", read_only = True, width = 300)
-    run_time = ft.TextField(label = "Time", read_only = True, width = 300)
-    txt_time = ft.Text("")  
-    txt_date = ft.Text("")
+    name_dropdown = ft.Dropdown(
+        label = "Name",
+        width = 300,
+        options = [
+            ft.dropdown.Option("Jett"),
+            ft.dropdown.Option("Skye"),
+            ft.dropdown.Option("Reyna"),
+        ]
+    )
 
-    gc = gspread.service_account(filename='C:/Users/crenz/Documents/GitHub/Attendance-App/src/credentials.json')
-
-    sh = gc.open_by_key("1X_OgCH1dKqftUKEPXluDsUk6plmh8B0nvIkWyI-BiKs")
-    print(sh.sheet1.get('A1'))
-    
     dept_dropdown = ft.Dropdown(
         label = "Department",
         width = 300,
@@ -30,58 +35,73 @@ def main(page: ft.Page):
             ft.dropdown.Option("QA Tester"),
         ]
     )
+    run_date = ft.TextField(label = "Date", read_only = True, width = 300)
+    run_time = ft.TextField(label = "Time", read_only = True, width = 300)
+    txt_time = ft.Text("")  
+    txt_date = ft.Text("")
+
+    #FUNCTION TO GET THE NEXT EMPTY ROW in COLUMN A1(1st COLUMN)
+    def get_next_empty_cell():
+        col = worksheet.col_values(1)
+        next_empty_row = len(col) + 1
+        return next_empty_row
+
 
     def confirm_btn(e):
         page.close(dlg_modal)
 
     dlg_modal = ft.AlertDialog(
         modal = True,
-        title = ft.Text("Please Confirm LOG Data"),
-        content = ft.Column([]),
+        title = ft.Text("Good Day Worker"),
+        content = ft.Text("You've Succesfully Clock-in"),
         actions = [
             ft.TextButton("Confirm", on_click = confirm_btn),
         ]
     )
-
-    def open_modal(e):
-        name = txt_name.value
+    
+    #FUNCTION TO RECIRD ATTENDANCE TO GOOGLE SHEET
+    def clock_in(e):
+        name = name_dropdown.value
         dept = dept_dropdown.value
         cur_date = datetime.now().strftime("%Y-%m-%d")
-        txt_date.value = f"Time in: {cur_date}"
-
         cur_time = datetime.now().strftime("%H:%M:%S")
-        txt_time.value = f"Time in: {cur_time}"
 
         if cur_time >= "18:01:00":
-            status = "Time-Out"
-        else:
-            status = "Time-In"
+            timeout = cur_time()
+            status = "Out"
+        elif cur_time >= "09:00:00":
+            timeout = "Pending" 
+            status = "In"
 
-        dlg_modal.content.controls = [
+        '''dlg_modal.content.controls = [
             ft.Text(f"Name: {name}"),
             ft.Text(f"Department: {dept}"),
             txt_date,
             txt_time,
-            ft.Text(f"Status: {status}"),
-        ]
+            #ft.Text(f"Status: {status}"),
+        ]'''
+        next_empty_cell = get_next_empty_cell()
+
+        worksheet.update(range_name = f"A{next_empty_cell}:F{next_empty_cell}" , values=[[name,dept,cur_time,timeout,status,cur_date]])
         page.open(dlg_modal)
 
     btn_clock = ft.ElevatedButton(
         "Time in",
-        on_click = open_modal,
+        on_click = clock_in,
         width = 200,
         height = 40, 
     )
 
     page.add(
         txt,
-        txt_name,
+        name_dropdown,
         dept_dropdown,
         run_date,
         run_time,
         btn_clock,
     )
 
+    #FUNCTION TO SHOW REAL-TIME SYSTEM CLOCK AND UPDATE BUTTON BASED ON TIME
     while True:
         current_date = datetime.now().strftime("%Y-%m-%d")
         current_time = datetime.now().strftime("%H:%M:%S")
@@ -90,11 +110,12 @@ def main(page: ft.Page):
         
         if current_time >= "18:01:00":
             btn_clock.text = "Time-Out"
-        elif current_time >= "00:01:00":
+        #elif current_time >= "10:25:00":
+            #btn_clock.disabled = True
+        elif current_time >= "09:00:00":
             btn_clock.text = "Time-in"
 
         page.update()
         time.sleep(1)
-
 
 ft.app(main)
